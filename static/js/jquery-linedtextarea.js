a0c3d9d25093103fc8d5f0a904a59e93f9a1810e
Jimmy Cao <jc2462@cornell.edu>
2016-09-07 18:07:14 -0400

    Modified lined text area.

--- static/js/jquery-linedtextarea.js
+++ static/js/jquery-linedtextarea.js
@@ -39,13 +39,15 @@
 		 * Helper function to make sure the line numbers are always
 		 * kept up to the current system
 		 */
-		var fillOutLines = function(codeLines, h, lineNo){
+		var fillOutLines = function(codeLines, h, lineNo, ccodeLines){
 			while ( (codeLines.height() - h ) <= 0 ){
-				if ( lineNo == opts.selectedLine )
+				if ( lineNo == opts.selectedLine ) {
 					codeLines.append("<div class='lineno lineselect'>" + lineNo + "</div>");
-				else
+					ccodeLines.append("<div class='lineno lineselect'>" + lineNo + "</div>");
+                } else { 
 					codeLines.append("<div class='lineno'>" + lineNo + "</div>");
-				
+					ccodeLines.append("<div class='lineno'>" + lineNo + "</div>");
+                }
 				lineNo++;
 			}
 			return lineNo;
@@ -70,15 +72,21 @@
 			var linedWrapDiv 			= linedTextAreaDiv.parent();
 			
 			linedWrapDiv.prepend("<div class='lines' style='width:50px'></div>");
+            linedWrapDiv.parent().append("<div class='clinesWrap' style='width:50px;'><div id='clines' style='width:50px;'></div></div>");
 			
 			var linesDiv	= linedWrapDiv.find(".lines");
+			var clinesDiv	= linedWrapDiv.parent().find("#clines");
+			linedWrapDiv.parent().find(".clinesWrap").height( textarea.height() + 6 );
 			linesDiv.height( textarea.height() + 6 );
+            clinesDiv.height( 3*(textarea.height() + 6)/2 );
 			
 			
 			/* Draw the number bar; filling it out where necessary */
 			linesDiv.append( "<div class='codelines'></div>" );
+			clinesDiv.append( "<div class='codelines'></div>" );
 			var codeLinesDiv	= linesDiv.find(".codelines");
-			lineNo = fillOutLines( codeLinesDiv, linesDiv.height(), 1 );
+			var ccodeLinesDiv	= clinesDiv.find(".codelines");
+			lineNo = fillOutLines( codeLinesDiv, linesDiv.height(), 1 , ccodeLinesDiv );
 
 			/* Move the textarea to the selected line */ 
 			if ( opts.selectedLine != -1 && !isNaN(opts.selectedLine) ){
@@ -105,7 +113,8 @@
 				var scrollTop 		= domTextArea.scrollTop;
 				var clientHeight 	= domTextArea.clientHeight;
 				codeLinesDiv.css( {'margin-top': (-1*scrollTop) + "px"} );
-				lineNo = fillOutLines( codeLinesDiv, scrollTop + clientHeight, lineNo );
+				ccodeLinesDiv.css( {'margin-top': (-1*scrollTop) + "px"} );
+				lineNo = fillOutLines( codeLinesDiv, scrollTop + clientHeight, lineNo, ccodeLinesDiv);
 			});
 
 
@@ -113,6 +122,7 @@
 			textarea.resize( function(tn){
 				var domTextArea	= $(this)[0];
 				linesDiv.height( domTextArea.clientHeight + 6 );
+				clinesDiv.height( domTextArea.clientHeight + 6 );
 			});
 
 		});
